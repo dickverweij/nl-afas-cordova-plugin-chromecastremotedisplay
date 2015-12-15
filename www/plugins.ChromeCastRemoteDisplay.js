c26eac6906c34430d6ef29cea376bf57b5702ef1
afasdev <d.verweij@afas.nl>
2015-12-15 10:46:19 +0100

    fixed plugin

--- www/plugins.ChromeCastRemoteDisplay.js
+++ www/plugins.ChromeCastRemoteDisplay.js
@@ -1,49 +1,47 @@
-cordova.define("nl.afas.cordova.plugin.chromeCastRemoteDisplay.ChromeCastRemoteDisplay", function (require, exports, module) { /*jslint browser: true, devel: true, node: true, sloppy: true, plusplus: true*/
-    /*global require, cordova */
-    /*
-    The MIT License (MIT)
+/*
+The MIT License (MIT)
     
-    Copyright (c) 2015 Dick Verweij, dickydick1969@hotmail.com, AFAS Software  - d.verweij@afas.nl
+Copyright (c) 2015 Dick Verweij, dickydick1969@hotmail.com, AFAS Software  - d.verweij@afas.nl
     
-    Permission is hereby granted, free of charge, to any person obtaining a copy of
-    this software and associated documentation files (the "Software"), to deal in
-    the Software without restriction, including without limitation the rights to
-    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-    the Software, and to permit persons to whom the Software is furnished to do so,
-    subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
     
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
     
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-    */
-    var exec = require('cordova/exec');
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+var exec = require('cordova/exec');
 
-    function ChromeCastRemoteDisplay() { }
+function ChromeCastRemoteDisplay() { }
 
-    ChromeCastRemoteDisplay.prototype.getSessions = function (publishId) {
-        return new Promise(function (resolve, reject) {
-            exec(resolve, reject, 'ChromeCastRemoteDisplay', 'getSessions', [publishId]);
-        });
-    };
+ChromeCastRemoteDisplay.prototype.getSessions = function (publishId) {
+    return new Promise(function (resolve, reject) {
+        exec(resolve, reject, 'ChromeCastRemoteDisplay', 'getSessions', [publishId]);
+    });
+};
 
-    ChromeCastRemoteDisplay.prototype.startCast = function (session) {
-        return new Promise(function (resolve, reject) {
-            exec(resolve, reject, 'ChromeCastRemoteDisplay', 'startCast', [session]);
-        });
-    };
+ChromeCastRemoteDisplay.prototype.startCast = function (session) {
+    return new Promise(function (resolve, reject) {
+        exec(resolve, reject, 'ChromeCastRemoteDisplay', 'startCast', [session]);
+    });
+};
 
-    ChromeCastRemoteDisplay.prototype.endCast = function (session) {
-        return new Promise(function (resolve, reject) {
-            exec(resolve, reject, 'ChromeCastRemoteDisplay', 'endCast', [session]);
-        });
-    };
+ChromeCastRemoteDisplay.prototype.endCast = function (session) {
+    return new Promise(function (resolve, reject) {
+        exec(resolve, reject, 'ChromeCastRemoteDisplay', 'endCast', [session]);
+    });
+};
 
-    window.ChromeCastRemoteDisplay = new ChromeCastRemoteDisplay();
+window.ChromeCastRemoteDisplay = new ChromeCastRemoteDisplay();
 
-});
+
